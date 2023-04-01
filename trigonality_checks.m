a1bc237e008f20757847c4c84391a43bb9f1fc36
orlic1 <50557788+orlic1@users.noreply.github.com>
2023-04-01 22:54:31 +0200

    Update trigonality_checks.m

--- trigonality_checks.m
+++ trigonality_checks.m
@@ -12,7 +12,7 @@ Genus4GonalMap(X);
 
 X, ws, pairs:=eqs_quos(84,[[84]]);
 Xw:=pairs[1,1];
-pts:=PointSearch(Xw,20);
+pts:=PointSearch(Xw,100);
 plcs:=[Place(p): p in pts];
 assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
 //this proves that X_0^+(84) is trigonal over Q
@@ -39,8 +39,6 @@ assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
 //this proves that X_0^+(116) is trigonal over Q
 
 X, ws, pairs:=eqs_quos(129,[[129]]);
-is_trigonal(pairs[1,1]);
-//returns true
 Xw:=pairs[1,1];
 pts:=PointSearch(Xw,100);
 plcs:=[Place(p): p in pts];
@@ -48,33 +46,53 @@ assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
 //this proves that X_0^+(129) is trigonal over Q
 
 X, ws, pairs:=eqs_quos(137,[[137]]);
+Xw:=pairs[1,1];
 pts:=PointSearch(Xw,100);
 plcs:=[Place(p): p in pts];
 assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
 //this proves that X_0^+(137) is trigonal over Q
 
 X, ws, pairs:=eqs_quos(155,[[155]]);
-pts:=PointSearch(Xw,20);
+Xw:=pairs[1,1];
+pts:=PointSearch(Xw,100);
 plcs:=[Place(p): p in pts];
 assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
 //this proves that X_0^+(155) is trigonal over Q
 
 X, ws, pairs:=eqs_quos(159,[[159]]);
-is_trigonal(pairs[1,1]);
-//true
 Xw:=pairs[1,1];
 pts:=PointSearch(Xw,100);
 plcs:=[Place(p): p in pts];
 assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
 // this proves that X_0^+(159) is trigonal over Q
 
-// Now we prove Propostion 4.16, by showing that quotients of X_0(N) are not trigonal by showing that there are no degree 3 functions over F_p, where p is a prime of good reduction.
+// Now we prove Propostion 4.17, by showing that quotients of X_0(N) are not trigonal by showing that there are no degree 3 functions over F_p, where p is a prime of good reduction.
+
+X, ws, pairs:=eqs_quos(110,[[55]]);
+Xw:=pairs[1,1];
+D:=DefiningEquations(Xw);
+D2:=[];
+for i:=1 to #D do
+p:=D[i];
+l:=LCM([Denominator(a):a in Coefficients(p)]);
+p:=p*l;
+D2:=D2 cat [p];
+end for;
+C:=Scheme(ProjectiveSpace(Rationals(),Genus(Xw)-1),D2);
+C2:=ChangeRing(C,GF(7));
+FF := FunctionField(C2);
+AFF := AlgorithmicFunctionField(FF);
+pls1:=Places(AFF,1);
+pls2:=Places(AFF,2);
+#pls1,#pls2; 
+pls:=Places(AFF,3);
+Max({Dimension(RiemannRochSpace(p)):p in Places(AFF,3)});
+Max({Dimension(RiemannRochSpace(p+q)):p in pls1, q in pls2});
+Max({Dimension(RiemannRochSpace(p+q+u)):p,q,u in pls1});
+//returns 1 in all cases, proving that the quotient is not trigonal 
 
 X, ws, pairs:=eqs_quos(145,[[29]]);
-pts:=PointSearch(Xw,100);
-plcs:=[Place(p): p in pts];
-assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
-//does not find any functions
+Xw:=pairs[1,1];
 D:=DefiningEquations(Xw);
 D2:=[];
 for i:=1 to #D do
@@ -96,14 +114,8 @@ Max({Dimension(RiemannRochSpace(p+q)):p in pls1, q in pls2});
 Max({Dimension(RiemannRochSpace(p+q+u)):p,q,u in pls1});
 //returns 1 in all cases, proving that the quotient is not trigonal 
 
-
-
 X, ws, pairs:=eqs_quos(161,[[161]]);
 Xw:=pairs[1,1];
-pts:=PointSearch(Xw,100);
-plcs:=[Place(p): p in pts];
-assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
-// fails
 D:=DefiningEquations(Xw);
 D2:=[];
 for i:=1 to #D do
@@ -126,13 +138,7 @@ Max({Dimension(RiemannRochSpace(p+q+u)):p,q,u in pls1});
 //returns 1 in all cases, proving that the quotient is not trigonal 
 
 X, ws, pairs:=eqs_quos(173,[[173]]);
-is_trigonal(pairs[1,1]);
-//true
 Xw:=pairs[1,1];
-pts:=PointSearch(Xw,100);
-plcs:=[Place(p): p in pts];
-assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
-// fails
 D:=DefiningEquations(Xw);
 D2:=[];
 for i:=1 to #D do
@@ -155,13 +161,7 @@ Max({Dimension(RiemannRochSpace(p+q+u)):p,q,u in pls1});
 //returns 1 in all cases, proving that the quotient is not trigonal 
 
 X, ws, pairs:=eqs_quos(177,[[59]]);
-is_trigonal(pairs[1,1]);
-//true
 Xw:=pairs[1,1];
-pts:=PointSearch(Xw,100);
-plcs:=[Place(p): p in pts];
-assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
-// fails
 D:=DefiningEquations(Xw);
 D2:=[];
 for i:=1 to #D do
@@ -184,13 +184,7 @@ Max({Dimension(RiemannRochSpace(p+q+u)):p,q,u in pls1});
 //returns 1 in all cases, proving that the quotient is not trigonal 
 
 X, ws, pairs:=eqs_quos(188,[[47]]);
-is_trigonal(pairs[1,1]);
-//true
 Xw:=pairs[1,1];
-pts:=PointSearch(Xw,100);
-plcs:=[Place(p): p in pts];
-assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
-// fails
 D:=DefiningEquations(Xw);
 D2:=[];
 for i:=1 to #D do
@@ -205,19 +199,15 @@ FF := FunctionField(C2);
 AFF := AlgorithmicFunctionField(FF);
 pls1:=Places(AFF,1);
 pls2:=Places(AFF,2);
-#pls1,#pls2; // 3 12
+#pls1,#pls2;
 pls:=Places(AFF,3);
 Max({Dimension(RiemannRochSpace(p)):p in Places(AFF,3)});
+Max({Dimension(RiemannRochSpace(p+q)):p in pls1, q in pls2});
+Max({Dimension(RiemannRochSpace(p+q+u)):p,q,u in pls1});
 //returns 1 in all cases, proving that the quotient is not trigonal 
 
 X, ws, pairs:=eqs_quos(199,[[199]]);
-is_trigonal(pairs[1,1]);
-//true
 Xw:=pairs[1,1];
-pts:=PointSearch(Xw,100);
-plcs:=[Place(p): p in pts];
-assert Maximum([Dimension(RiemannRochSpace(p+q+r)):p,q,r in plcs]) eq 2;
-// fails
 D:=DefiningEquations(Xw);
 D2:=[];
 for i:=1 to #D do
@@ -232,7 +222,7 @@ FF := FunctionField(C2);
 AFF := AlgorithmicFunctionField(FF);
 pls1:=Places(AFF,1);
 pls2:=Places(AFF,2);
-#pls1,#pls2; // 6 11
+#pls1,#pls2;
 pls:=Places(AFF,3);
 Max({Dimension(RiemannRochSpace(p)):p in Places(AFF,3)});
 Max({Dimension(RiemannRochSpace(p+q)):p in pls1, q in pls2});
